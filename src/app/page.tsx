c631a58c5e08e3292b9bad1fa7933d035020db4c
thylakoids <liyulongsjtu@hotmail.com>
2024-03-03 14:26:47 +0800

    chore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { prisma } from "@/db";
 import TodoItem from "@/components/TodoItem";
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 function getTodos() {
   return prisma.todo.findMany();
 }
@@ -14,7 +16,6 @@ async function toggleTodo(id: string, complete: boolean) {
   });
 }
 
-export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const todos = await getTodos();
